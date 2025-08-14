5a0dbd1b8364c7bb11c3a3f00c474a2ff8476c31
ryanjblajda <ryan.j.blajda@gmail.com>
2025-08-13 21:13:48 -0400

    parse qsc files

---
+++ Parsing/QSC/QSCParser.cs
@@ -0,0 +1,19 @@
+﻿using TeamsFileNotifier.Global;
+using TeamsFileNotifier.Messaging;
+using TeamsFileNotifier.Parsing.Crestron;
+
+namespace TeamsFileNotifier.Parsing.QSC
+{
+    internal class QSCParser : GenericParser
+    {
+        private static List<string> _validExtensions = new List<string>() { };
+
+        public QSCParser() : base() { }
+
+        internal override void OnFileChanged(FileChangedMessage message)
+        {
+            //we only want to parse valid extensions for us
+            if (_validExtensions.Contains(Path.GetExtension(message.FilePath))) { /* DO STUFF */ }
+        }
+    }
+}
