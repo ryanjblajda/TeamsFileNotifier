6b50c250dae6909aaa5fcee49352e4cb410cb69f
ryanjblajda <ryan.j.blajda@gmail.com>
2025-08-13 21:12:49 -0400

    parse custom files or generate custom messages

---
+++ Parsing/Custom/CustomParser.cs
@@ -0,0 +1,19 @@
+﻿using TeamsFileNotifier.Global;
+using TeamsFileNotifier.Messaging;
+using Serilog;
+using System.Text;
+
+namespace TeamsFileNotifier.Parsing.Custom
+{
+    internal class CustomParser : GenericParser
+    {
+        public CustomParser() : base() { }
+
+        internal override void OnFileChanged(FileChangedMessage message)
+        {
+            string content = String.Empty;
+            //if we have a custom action string to generate the format, do so
+            if (message.CustomAction != String.Empty) { content = GenerateCustomActionContent(message.CustomAction); }
+        }
+    }
+}
