4e2cee184783a9a162d3e1159cd3ecaddb02c26a
ryanjblajda <ryan.j.blajda@gmail.com>
2025-08-21 13:44:39 -0400

    fix: add function to sanitize input from the json

---
+++ Configuration/Sanitizer.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace teams_file_notifier.Configuration
+{
+    internal static class Sanitizer
+    {
+        internal static string SanitizeFileContents(string content)
+        {
+            string result = content;
+            
+            Log.Information($"Sanitizer | attempting to sanitize file path strings");
+
+            try {  result = Regex.Replace(content, @"(?<=""path""\s*:\s*"")(.*?)(?="")", match =>
+                {
+                    string path = match.Value;
+                    //only replace single backslashes, that have no preceding backslash, preventing previously fixed strings from being falsely fixed twice
+                    return Regex.Replace(path, @"(?<!\\)\\(?![\\])", @"\\");
+                });
+            }
+            catch(Exception e) { Log.Warning($"Sanitizer | failed to sanitize input -> {e.Message}"); }
+            finally { }
+            
+            return result;
+        }
+    }
+}
